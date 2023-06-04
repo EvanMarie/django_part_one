d385e3807373c65ff5d134abdb5c7577203a0f9b
Evan Marie Carr <evanmariecarr@gmail.com>
2023-06-04 17:04:44 -0400

    up through one-to-many

---
+++ store/models.py
@@ -0,0 +1,98 @@
+from django.db import models
+
+
+# Product to Promotion: a promotion can have multiple products, 
+# and a product can have multiple promotions
+class Promotion(models.Model):
+    
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    # This makes the sku the primary key/unique id and Django will not create an id field
+    # sku = models.CharField(max_length=10, primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    # if a collection is deleted, PROTECT will prevent the deletion of the products within
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+
+
+class Customer(models.Model):
+    # Creating a variable for the default membership to simplify any changes in the future
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    # Creating a list of tuples for the membership choices
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    birth_date = models.DateField(null=True)
+    # Choice field - https://docs.djangoproject.com/en/3.2/ref/models/fields/#choices
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+
+    # auto_now_add - the first time the object is created, set the value to the current
+    # date/time
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    # if a customer is deleted, PROTECT will prevent the deletion of the orders within
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    # if an order is deleted, PROTECT will prevent the deletion of the order items within
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    # if a product is deleted, PROTECT will prevent the deletion of the order items within
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    # product prices can change. This stores the price at the time of the order
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+# one-to-one relationship between customers and addresses
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    # if the customer to address is one-to-one, i.e. they can have one address
+    # customer = models.OneToOneField(
+    #     Customer, on_delete=models.CASCADE, primary_key=True)
+    # to allow customers more than one address: One-to-many relationship
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    # if a cart is deleted, all items within will be deleted as well
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    # if a product is deleted, it will be deleted from all shopping carts as well
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
